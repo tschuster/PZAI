fbe9446b4711d28a58468a27b9f4190b0c7f0831
Thies Schuster <schuster@redrauscher.de>
2013-07-12 22:10:09 +0200

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-PZAI
-====
-
-(Rapsberry) Pi Zombie AI
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pzai::Application.config.secret_key_base = 'd3ba23c0f2a76f6d7f61b09c4a034404883faaa80bf1f7fec9043d2c75e8fa7c51cf002b23efac87faff238e44e206ee52e76c09e7bebfc149b5ac11aff219ab'
